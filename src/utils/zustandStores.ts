606bd62efdbccf95ebad1e9124c99466061f843b
lalatina11 <rahmadancandra@gmail.com>
2025-05-31 16:40:26 +0700

    feat: add editor store

--- src/utils/zustandStores.ts
+++ src/utils/zustandStores.ts
@@ -23,3 +23,48 @@ export const useAuthStore = create((persist<AuthStore>(
     storage: createJSONStorage(() => sessionStorage)
 })))
 
+type TextOption = {
+    text: string;
+    fontSize: number;
+    color: string;
+    top: number;
+    left: number;
+}
+
+interface CanvasOption {
+    height: number;
+    orientation: string;
+    size: string;
+    backgroundColor: string;
+}
+
+export type EditorStore = {
+    selectedLayer: "canvas" | "text"
+    textOption: TextOption
+    canvasOption: CanvasOption
+    setSelectedLayer: (newLayer: EditorStore['selectedLayer']) => void
+    setTextOption: (newOption: TextOption) => void
+    addText: () => void
+    setCanvasOption: (newCanvasOpt: CanvasOption) => void
+}
+
+export const useEditorStore = create<EditorStore>((set) => ({
+    selectedLayer: "canvas",
+    textOption: {
+        text: "",
+        fontSize: 48,
+        color: "#000000",
+        top: 0,
+        left: 0
+    },
+    canvasOption: {
+        height: 0,
+        orientation: "",
+        size: "original",
+        backgroundColor: "#008080"
+    },
+    setSelectedLayer: (newLayer: EditorStore['selectedLayer']) => set({ selectedLayer: newLayer }),
+    setTextOption: (newOption: EditorStore['textOption']) => set({ textOption: newOption }),
+    addText: () => set(prev => ({ textOption: { ...prev.textOption, text: "Add Text" } })),
+    setCanvasOption: (newCanvasOpt: CanvasOption) => set({ canvasOption: newCanvasOpt })
+}))
